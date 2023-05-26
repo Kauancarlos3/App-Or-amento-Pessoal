77b98d1eebf2fe845687dcdce08267c45f1a1ee8
Kauancarlos3 <carlos_kauan33@hotmail.com>
2023-05-26 09:12:16 -0300

    botões de exclusão

--- app.js
+++ app.js
@@ -54,13 +54,14 @@ class BancoDados {
             //recuperar a despesa
             let despesa = JSON.parse(localStorage.getItem(i));
 
-            despesas.push(despesa);
-
             //existe a possibilidade de existir índices que foram pulados/removidos
             //neste caso vamos pular esse índices
             if (despesa === null) {
                 continue
             }
+
+            despesa.id = i;
+            despesas.push(despesa);
         }
         return despesas;
     }
@@ -112,6 +113,11 @@ class BancoDados {
 
         return despesasFiltradas
     }
+
+    remover(id){
+        localStorage.removeItem(id)
+    }
+
 }//Gravará as despesas no localStorage
 
 let bancoDados = new BancoDados()
@@ -200,6 +206,22 @@ function carregarListaDespesa(despesas = Array(), filtro = false) {
         linha.insertCell(1).innerHTML = d.tipo
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
+        
+        //criar o botão para excluir
+        let btn = document.createElement("button")
+        btn.className = 'btn btn-danger'
+        btn.innerHTML = '<i class = "fas fa-times"></i>'
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function(){
+            let id = this.id.replace('id_despesa_', '')
+            console.log(d)
+            console.log(id)
+            bancoDados.remover(id)
+
+            window.location.reload()
+        }
+        linha.insertCell(4).append(btn)
+        
 
     });
 }
